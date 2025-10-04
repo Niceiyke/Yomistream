c0d98d8f4177b1acc889beac7d467360b179975c
diyke04 <iykedave04@gmail.com>
2025-10-04 02:10:53 +0100

    cc

--- Backend/app/services/clipper/service.py
+++ Backend/app/services/clipper/service.py
@@ -155,6 +155,8 @@ class ClipperService:
                 '-o', output,
                 '--no-warnings',
                 '--no-check-certificate',
+                '--extractor-args', 'youtube:player_client=android',
+                '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 str(url)
             ]
             
